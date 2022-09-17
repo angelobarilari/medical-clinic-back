7ed644001ab6329ac7dc9717783d3f0c06a2a7e5
angel <angelomelo_tdai@hotmail.com>
2022-09-17 00:44:15 -0300

    novas files

--- src/data-source.js
+++ src/data-source.js
@@ -6,11 +6,19 @@ export const database = new Client(
     ? {
         user: "postgres",
         host: "localhost",
-        database: "tests_products",
+        database: process.env.POSTGRES_DB_TEST,
         password: "1234",
         port: 5432,
       }
     : {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+        synchronize: false,
+        logging: true,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
--- src/routes/appointments.routes.js
+++ src/routes/appointments.routes.js
@@ -5,15 +5,17 @@ import deleteAppointmentController from "../controllers/appointment/deleteAppoin
 import listAppointmentByNameController from "../controllers/appointment/listappointmentByName.controller";
 import listAppointmentsController from "../controllers/appointment/listAppointments.controller";
 import updateAppointmentController from "../controllers/appointment/updateAppointment.controller";
+import authAdm from "../middlewares/authAdm.middleware";
+import authUser from "../middlewares/authToken.middleware";
 
 const routes = Router()
 
 export const appointmentRoutes = () => {
-    routes.get("", listAppointmentByNameController)
-    routes.get("/all", listAppointmentsController)
-    routes.post("", createAppointmentController)
-    routes.patch("/:id", updateAppointmentController)
-    routes.delete("/:id", deleteAppointmentController)
+    routes.get("", authUser, authAdm, listAppointmentByNameController)
+    routes.get("/all", authUser, authAdm, listAppointmentsController)
+    routes.post("", authUser, authAdm, createAppointmentController)
+    routes.patch("/:id", authUser, authAdm, updateAppointmentController)
+    routes.delete("/:id", authUser, authAdm, deleteAppointmentController)
 
     return routes
 }
